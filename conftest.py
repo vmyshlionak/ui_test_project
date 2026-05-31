b4acc5d057ef62d1ac72e7286c6698d3384398fe
Viktoria Myshlionok <vmyshlionok@gmail.com>
2026-05-31 23:19:13 +0300

    more_args

--- conftest.py
+++ conftest.py
@@ -11,6 +11,19 @@ def driver():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--start-maximized')
+    options.add_argument('--no-sandbox')  # КРИТИЧНО для Docker/Linux
+    options.add_argument('--disable-dev-shm-usage')  # КРИТИЧНО для Docker
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--remote-debugging-port=9222')
+
+    # Важно: добавить аргументы для устранения ошибок сессии
+    options.add_argument('--disable-crash-reporter')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-in-process-stack-traces')
+    options.add_argument('--disable-logging')
+    options.add_argument('--log-level=3')  # только критические ошибки
+    options.add_argument('--silent')
     chrome_driver = webdriver.Chrome(options=options)
     return chrome_driver
 
