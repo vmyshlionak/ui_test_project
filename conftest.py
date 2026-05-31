ac61484b6f7423492e7633299722185715e2bcc0
Viktoria Myshlionok <vmyshlionok@gmail.com>
2026-05-31 20:20:24 +0300

    headless_mode_added

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from pages.customer_login import CustomerLogin
 from pages.triangle_page import TrianglePage
@@ -7,7 +8,9 @@ from pages.triangle_page import TrianglePage
 
 @pytest.fixture
 def driver():
-    chrome_driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_driver = webdriver.Chrome(options=options)
     return chrome_driver
 
 @pytest.fixture
