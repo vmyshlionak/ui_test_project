249c381b3799f5c32b0917ce459fe10e7aa68af9
Viktoria Myshlionok <vmyshlionok@gmail.com>
2026-06-02 20:29:00 +0300

    allure_attachments

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+import allure
 from pages.customer_login import CustomerLogin
 from pages.triangle_page import TrianglePage
 
@@ -15,8 +16,7 @@ def driver():
     options.add_argument('--disable-dev-shm-usage')  # КРИТИЧНО для Docker
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('--remote-debugging-port=9222')
-
+    # options.add_argument('--remote-debugging-port=9222') #ломает pytest -v в пайчарме
     # Важно: добавить аргументы для устранения ошибок сессии
     options.add_argument('--disable-crash-reporter')
     options.add_argument('--disable-extensions')
@@ -25,7 +25,9 @@ def driver():
     options.add_argument('--log-level=3')  # только критические ошибки
     options.add_argument('--silent')
     chrome_driver = webdriver.Chrome(options=options)
-    return chrome_driver
+    yield chrome_driver
+    allure.attach(chrome_driver.get_screenshot_as_png(), name='screenshot', attachment_type=AttachmentType.PNG)
+
 
 @pytest.fixture
 def login_page(driver):
